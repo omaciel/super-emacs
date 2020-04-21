3dde3f06a99244ef358fa7826356616f75767046
Og B. Maciel <omaciel@ogmaciel.com>
2020-04-21 12:38:57 -0400

    Add keybinding for multiple-cursors.

--- .emacs.d/super-emacs/config-key-bindings-custom.el
+++ .emacs.d/super-emacs/config-key-bindings-custom.el
@@ -1,3 +1,14 @@
 ;; Place your customizations to 'config-key-bindings.el' here.
 ;; Future updates to super-emacs will very less likely change this file so that
 ;; your customizations can remain free of merge conflicts.
+
+;; When you have an active region that spans multiple lines,
+;; the following will add a cursor to each line:
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+
+;; When you want to add multiple cursors not based on
+;; continuous lines, but based on keywords in the buffer:
+
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
