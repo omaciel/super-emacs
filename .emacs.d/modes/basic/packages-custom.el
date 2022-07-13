15456cabbe9ecd55c2037594c560dad8e16220b8
Og B. Maciel <omaciel@ogmaciel.com>
2022-07-13 15:56:29 -0400

    Minor changes for golang.

--- .emacs.d/modes/basic/packages-custom.el
+++ .emacs.d/modes/basic/packages-custom.el
@@ -3,8 +3,14 @@
 ;; your customizations can remain free of merge conflicts.
 (defvar se/packages-custom-standard
   '(;; Related to golang
+    ;; Required for highlighting, indentation etc. It is major mode for Go.
     (go-mode github "dominikh/go-mode.el" t)
+    ;; LSP support for Emacs.
     (lsp-mode github "emacs-lsp/lsp-mode" t)
+    ;; Higher level UI modules for LSP Mode.
+    (lsp-ui github "lsp-ui" t)
+    ;; YASnippet is a template expansion system.
+    (yasnippet melpa "yasnippet" t)
     ;; Magit got git magic
     (magit melpa "magit" t)
     ;; Git-gutter
@@ -26,17 +32,17 @@
 ;; Default git-gutter for files in git repositories
 (global-git-gutter-mode +1)
 
-;; Load go-mode automatically when opening a .go file
-(autoload 'go-mode "go-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
-(require 'lsp-mode)
-(add-hook 'go-mode-hook #'lsp-deferred)
+;; Company mode
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-length 1)
+
+;; Go - lsp-mode
 ;; Set up before-save hooks to format buffer and add/delete imports.
-;; Make sure you don't have other gofmt/goimports hooks enabled.
 (defun lsp-go-install-save-hooks ()
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
 (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
-(with-eval-after-load 'lsp-mode
-  ;; :global/:workspace/:file
-  (setq lsp-modeline-diagnostics-enable t))
+
+;; Start LSP Mode and YASnippet mode
+(add-hook 'go-mode-hook #'lsp-deferred)
+(add-hook 'go-mode-hook #'yas-minor-mode)
